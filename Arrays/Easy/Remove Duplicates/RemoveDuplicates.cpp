2d6222f25045cb42a1f358b14218c95688431efe
prakash079513 <prakashjc2705@gmail.com>
2025-05-27 00:13:33 +0530

    Remove Duplicates

--- Arrays/Easy/Remove Duplicates/RemoveDuplicates.cpp
+++ Arrays/Easy/Remove Duplicates/RemoveDuplicates.cpp
@@ -4,21 +4,21 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0; // If array is empty, no duplicates to remove
+        if (nums.empty()) return 0;
         
-        int index = 0; // Index to track where the next unique element should go
+        int index = 0; 
         
         for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i] != nums[index]) { // Found a new unique element
-                nums[++index] = nums[i]; // Place it at the next index
+            if (nums[i] != nums[index]) {
+                nums[++index] = nums[i]; 
             }
         }
-        return index + 1; // Length of the resulting array without duplicates
+        return index + 1;
     }
 };
 
 int main() {
-    // Example usage:
+  
     vector<int> nums = {1, 1, 2, 2, 3, 4, 4, 4, 5};
     Solution s;
     int len = s.removeDuplicates(nums);
