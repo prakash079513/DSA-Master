eef9d1f5f23f72d9a811c3dd1628e3d27260c056
prakash079513 <prakashjc2705@gmail.com>
2025-06-08 23:54:18 +0530

    2Sum

---
+++ Arrays/Medium/2Sum/2Sum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen; // value -> index
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (seen.count(complement)) {
+                return {seen[complement], i};
+            }
+            seen[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    Solution s1;
+    vector<int> result = s1.twoSum(nums, target);
+    for (int index : result) {
+        cout << index << " ";
+    }
+    return 0;
+}
